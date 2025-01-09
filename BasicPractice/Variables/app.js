edf04bfd7653359d365a844cc07061b8fe084f9f
iamarsalanofficial <iamarsalanofficial1@gmail.com>
2025-01-09 20:17:58 +0500

    Basic practice Variables.

---
+++ BasicPractice/Variables/app.js
@@ -0,0 +1,11 @@
+// Using let
+let name = "Arsalan"; // Tumhara naam store kar liya
+console.log(name); // Output: Arsalan
+
+// Using const
+const pi = 3.14; // Yeh kabhi change nahi hoga
+console.log(pi); // Output: 3.14
+
+// Using var (Old style)
+var age = 25; // Umar store ki
+console.log(age); // Output: 25
